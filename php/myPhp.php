054ebd7586b5f601826af737b9db5294dc569f10
amir <ahghavam@gmail.com>
2019-06-03 18:53:59 +0430

    complete database

--- php/myPhp.php
+++ php/myPhp.php
@@ -1,7 +1,33 @@
 <?php
 
-echo "boro baba chi mig";
+require 'rb.php';
 
-echo "asdf";
+$area= $_REQUEST["area"];
+
+// the password is 1234
+$mysql = mysqli_connect("localhost", "root", "1234", "reyhoon") or die(mysqli_connect_error());
+$query = "";
+$resualt = mysqli_query($mysql, $query);
+
+
+R::setup('mysql:host=localhost;dbname=ProjectOfReyhoon', 'root', '1234');
+R::wipe('category');
+$category = R::dispense('category');
+$category->id = "shandiz";
+$category->name = "shandiz";
+$idOne = R::store($category);
+
+$category = R::dispense('category');
+$category->id = "khaneye pizaa";
+$id2 = R::store($category);
+
+echo $idOne."salam ".$id2;
+$allCategories = R::find('category');
+foreach($allCategories as $b){
+    echo "name of resturant: ".$b->id."\n";
+}
+R::close();
+
+
 
 ?>
